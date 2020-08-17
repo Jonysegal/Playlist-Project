b7d4ff4c8dd5bdae87f00d5ade8579d2ebdd6164
Jonathan Segal <jonysegal@gmail.com>
2020-08-17 02:12:39 -0400

    Update run, mostly used for testing rn

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 from UserControl import UserController
 userControl = UserController()
 userControl.get_playlist_choice()
+#userControl.get_requested_time()
+userControl.get_playlist_length(userControl.lastSelectedPlaylist)
