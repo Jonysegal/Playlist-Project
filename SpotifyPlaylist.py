b67f8d102c2adf9389639a1b45924f7b53fd7b10
Jonathan Segal <jonysegal@gmail.com>
2020-08-17 00:44:39 -0400

    Change printplaylists to get playlists
    Also remove some old debug stuff

--- SpotifyPlaylist.py
+++ SpotifyPlaylist.py
@@ -3,7 +3,7 @@ import urllib.parse
 import json
 
 class Runner: 
-    api_token = 'BQB3beJKtsOCWFVP8EgSsyiFOMJ6NyY1pE-Rz4Ff0zdtFOkuKzqITF3WcshWygOriyoVpDexXGA-WjqC5OIaH6m8reZHtg8rXGxRdPb6RTmGmfTDpKiTDfHItujwHMbO-4cIZf20zSaLERy-xVNNPqJNxEgrPwGkdEAVtaLVQxCiNlzcGrDvLzeELd6s-LduSA9X54actrPk7gxxKj6Q1dn088_hpVCQO1YoJxz7XlCHYGjcwQCXPX9oGo69kGFC1XsRtaPLVqAwEb46kN7STKe2B6oXQ3OUMEQt'
+    api_token = 'BQBoow3WkJs7TLb8xR_NVG6lWzny9Jx-Zl3BcIl255tiWlnZwYARMKqgOTjUOKqiN6JxxaKqx8w0-COEHlN2ocKrTzaBBrfhTHfQ5WLk16K4rRkKVWDTErGwuaAXXxw1IFV9l1lxa_zeWi4Wf1_PkPkTZywI8z1QAZxqDlaiqZl63ZiArBvIkxxx78yYdkjYyNvsPwFHD5OCTPrnzyEMp7Uiz62s9ulAwaKTX9tXGBBz6cEs31HnVFUNYVoTloHBMsWi6GgHo7ZiGVIts5NGSVhioZKgj41hjKT6'
     user_id = 'ol0ys8y3412mh7tfh9n0lbprc'
     def search_song(self, artist, track):
 
@@ -54,19 +54,14 @@ class Runner:
         if not response.ok:
             print("adding song to playlist failed FUCK")
             print(response.json())
-    def print_playlists(self):
+    def get_playlists(self):
         url = "https://api.spotify.com/v1/me/playlists"
         response = requests.get(
             url,
             headers={"Authorization": f"Bearer {self.api_token}"
                 }
             )
-        print("printing playlists")
-        for x in response.json().get('items'):
-            print(x.get('name') + " " + x.get('id'))
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print(response.json().get('items')[10].get('name'))
-        return response.json().get('items')[10].get('id')
+        return response.json().get('items')
     def read_playlist(self, playlist_id):
         #note: can only get max of 100 items, but allowed to give an offset
         #therefore can iterate over full playlist by just popping up 100 in the offset until full thing is read
@@ -96,13 +91,3 @@ class Runner:
             print("couldn't make playlist called {}".format(name))
             print(response.json())
                
-        
-        
-            
-print("Not feeling great, I wish I was more important/ special to her")
-
-runner1 = Runner();
-runner1.make_new_playlist_called("YIZZLE IN THE TIZZLE")
-#runner1.search_song('Peter Gabriel', 'Modern Love')
-#runner1.add_song_to_playlist(runner1.search_song('Glass Animals', 'Life Itself'), '6hghRBZ4RJYyE5U7Pp4Ga5')
-#runner1.read_playlist(runner1.print_playlists())
